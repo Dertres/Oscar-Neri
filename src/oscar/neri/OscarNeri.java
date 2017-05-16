2bec0765fa158264810be04ef1ccfc909772bef2
T- <T-@PC259>
2017-05-15 19:41:38 -0500

    Mi primer clase de Java 2

---
+++ src/oscar/neri/OscarNeri.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oscar.neri;
+
+/**
+ *
+ * @author T-
+ */
+public class OscarNeri {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("¡Hola mundo!");//sout -> ctrl+spacebar->enter
+        
+    }
+    
+}
